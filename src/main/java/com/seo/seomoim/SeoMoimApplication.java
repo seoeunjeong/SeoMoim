e6ee91a10102810d355edf2a3056f39c0be472e7
서은정 <111293023+seoeunjeong@users.noreply.github.com>
2023-05-21 17:08:36 +0900

    first commit

---
+++ src/main/java/com/seo/seomoim/SeoMoimApplication.java
@@ -0,0 +1,13 @@
+package com.seo.seomoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoMoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoMoimApplication.class, args);
+	}
+
+}
